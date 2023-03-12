207add90115031194c336700a35de29909390ccb
BLAZE <>
2023-03-12 21:39:45 +0100

    the user service

---
+++ User Stock/src/test/java/com/userStockService/userService/UserStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userStockService.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
